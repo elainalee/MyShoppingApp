627f9017f41942e5f250dc22e82a19102ec49535
jennifer <jennlee0929@gmail.com>
2020-05-20 15:40:13 -0700

    added firebase functionality -signUp

--- app/src/main/java/leeJ/co/MyApp/SignUp.java
+++ app/src/main/java/leeJ/co/MyApp/SignUp.java
@@ -7,12 +7,17 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUp extends AppCompatActivity {
 
     // Variables
     TextInputLayout regName, regUserName, regEmail, regPhoneNum, regPassword;
     Button regButton, regToLogInButton;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,19 @@ public class SignUp extends AppCompatActivity {
         regButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("users");
+
+                // Get all the values
+                String name = regName.getEditText().getText().toString();
+                String username = regUserName.getEditText().getText().toString();
+                String email = regEmail.getEditText().getText().toString();
+                String phoneNum = regPhoneNum.getEditText().getText().toString();
+                String password = regPassword.getEditText().getText().toString();
+
+                UserHelperClass helperClass = new UserHelperClass(name, username, email, phoneNum, password);
+
+                reference.child(phoneNum).setValue(helperClass);
 
             }
         });
