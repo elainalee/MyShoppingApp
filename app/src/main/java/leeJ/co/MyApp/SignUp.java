e04897e61e0229c5a25f4e6891aec221195fa399
jennifer <jennlee0929@gmail.com>
2020-05-22 21:05:21 -0700

    MA-2 linked the button back to logIn screen

--- app/src/main/java/leeJ/co/MyApp/SignUp.java
+++ app/src/main/java/leeJ/co/MyApp/SignUp.java
@@ -2,6 +2,7 @@ package leeJ.co.MyApp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -42,6 +43,15 @@ public class SignUp extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 registerUser(view);
+            }
+        });
+
+        regToLogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LogIn.class);
+                startActivity(intent);
+
             }
         });
     }
