05d96e6aac58bf02aaf2ee2e7749b5718cab8a5e
jennifer <jennlee0929@gmail.com>
2021-02-05 16:16:43 -0800

    Added README.md

--- Flutter-Core/myapp_core/lib/database_handler/upload_handler.dart
+++ Flutter-Core/myapp_core/lib/database_handler/upload_handler.dart
@@ -7,49 +7,56 @@ import '../exceptions/wrong_price_format.dart';
 import '../common/database_constants.dart';
 
 Future<StorageUploadTask> uploadImageFile(File imageFile) async {
-    String fileName = basename(imageFile?.path ?? null);
-    StorageReference firebaseStorageRef =
-        FirebaseStorage.instance.ref().child('$ITEMS_IMAGES/$fileName');
-    StorageUploadTask uploadTask = firebaseStorageRef.putFile(imageFile ?? null);
-    return uploadTask;
-  }
+  String fileName = basename(imageFile?.path ?? null);
+  StorageReference firebaseStorageRef =
+      FirebaseStorage.instance.ref().child('$ITEMS_IMAGES/$fileName');
+  StorageUploadTask uploadTask = firebaseStorageRef.putFile(imageFile ?? null);
+  return uploadTask;
+}
 
 double priceTextToDouble(String priceText) {
   try {
-      double price;
-      price = double.parse(priceText);
-      return price;
-    } catch (err) {
-      throw WrongPriceFormatException();
-    }
-
+    double price;
+    price = double.parse(priceText);
+    return price;
+  } catch (err) {
+    throw WrongPriceFormatException();
+  }
 }
 
-void uploadListingData({String imageURL, String title, double price,
-  String category, String origin, String description, String sellerID}) {
-    final DatabaseReference databaseReference = FirebaseDatabase.instance.reference().child(ITEMS_SELL_TABLE);
+void uploadListingData(
+    {String imageURL,
+    String title,
+    double price,
+    String category,
+    String origin,
+    String description,
+    String sellerID}) {
+  final DatabaseReference databaseReference =
+      FirebaseDatabase.instance.reference().child(ITEMS_SELL_TABLE);
 
-    databaseReference.push().set({
-      I_TITLE: title, 
-      I_PRICE: price,
-      I_CATEGORY: category,
-      I_ORIGIN: origin,
-      I_DESCRIPTION: description,
-      I_IMAGE: imageURL ?? "",
-      I_SID: sellerID ?? ""
-    });
-  }
+  databaseReference.push().set({
+    I_TITLE: title,
+    I_PRICE: price,
+    I_CATEGORY: category,
+    I_ORIGIN: origin,
+    I_DESCRIPTION: description,
+    I_IMAGE: imageURL ?? "",
+    I_SID: sellerID ?? ""
+  });
+}
 
 String uploadToCart({String itemID, String userID}) {
   try {
-      DatabaseReference dbRef = FirebaseDatabase.instance.reference().child(CARTS_TABLE);
-      if (itemID != null && userID != null) {
-        dbRef.push().set({C_IID: itemID, C_UID: userID,});
-        return "this item is added to cart.";
-      } else {
-        return "Failed to add the item. Please try again.";
-      }
-    } catch (err) {
+    DatabaseReference dbRef =
+        FirebaseDatabase.instance.reference().child(CARTS_TABLE);
+    if (itemID != null && userID != null) {
+      dbRef.push().set({C_IID: itemID, C_UID: userID, "quantity": "1"});
+      return "this item is added to cart.";
+    } else {
       return "Failed to add the item. Please try again.";
     }
+  } catch (err) {
+    return "Failed to add the item. Please try again.";
+  }
 }
---
+++ README.md
@@ -0,0 +1,68 @@
+# What is "MyShoppingApp"?
+
+MyShoppingApp is an ecommerce app that helps the transaction between sellers and users(buyers).
+
+This app was initially designed with the hope of helping people selling items on their sns account by making the process easier to this app.
+
+To run the app, please refer to below (#To Run the App).
+
+
+
+# To Run the App
+
+1. Clone the entire project
+
+2. Open Android folder on Android Studio
+
+2. Go to Android/flutter_module on terminal
+    - cd flutter_module
+
+3. Run flutter packages get
+    - flutter packages get
+
+4. gradle sync on Android Studio, and run
+
+
+# In case of Error
+
+- Please check if you have flutter sdk and dart sdk.
+
+1. go to Android/flutter_module
+    - cd flutter_module
+
+2. clean the cache for flutter
+    - flutter clean
+
+3. Invalidate cache/restart on Android Studio
+
+4. get packages/ upgrade packages
+    - flutter packages get
+    - flutter packages upgrade
+
+5. sync gradle and run
+
+
+# How does this app work?
+
+When you log in as a user, please use the "New User? Sign Up" button. This will let you create your account to the database. Now, you can use the username and password to log in. When you log in, you will see all available listings. To change user information, you can click the profile button on top right. When you click on a listing, it leads you to ldp (listing detail page). You can press "cart", which will be stored on database. Also, you could click "See More of ...'s itmes >" button, which will let you see all available listings from the same seller.
+
+When you log in with the "are you a seller?" button, it leads you to the seller page. Seller account cannot be registered through the app. If you would like to check out, please use id: seller1, password: seller1pw. This will lead you to the seller page with the account. Here, you can click "My Listings" and look at the listings, and also add listing by clicking on "Add Listing" button below.
+
+
+# What did you use to develop the app?
+
+This app is written in Java (Android) and Dart (Flutter). 
+
+The app integrates Flutter into Android on run.
+
+Android consists of login page, listings page.
+
+Flutter includes ldp and smp, and those can be run on its own (read README.md under Flutter-Core folder).
+
+For database, I used Firebase Realtime Database and Firebase Storage (for image files).
+
+The database is constructed in regards to the ER diagram below.
+
+![ER Diagram](https://firebasestorage.googleapis.com/v0/b/my-app-leej.appspot.com/o/sample_images%2FER%20Diagram.png?alt=media&token=1a53def8-dedd-4926-93c6-becc07ccadfa)
+
+
